fef590fa55fae860562beba93f0f2571e1071d1d
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-07 22:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function mincost(arr)
 { 
+	arr = [...new Set(arr.sort((a,b)=>a-b));
 	if(arr.length===1){
 		return arr[0];
 	}
-	arr.sort((a,b)=>a-b);
+	
     let sum=0;
     for(let i=1;i<arr.length;i++){
         arr[i]+=arr[i-1]
