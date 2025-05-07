414e0cc36a0e2d8424693c4b50b0042e9f635980
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-07 21:58:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function mincost(arr)
 { 
+	if(arr.length===1){
+		return arr[0];
+	}
+	arr.sort((a,b)=>a-b);
+    let sum=0;
+    for(let i=1;i<arr.length;i++){
+        arr[i]+=arr[i-1]
+        sum+=arr[i]
+    }
+    return sum
 //write your code here
 // return the min cost
   
