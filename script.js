38e3e0b8cc6054e23c21624c924aee35b5c6a625
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-08 06:08:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 function mincost(arr)
 { 
-	// arr = [...new Set(arr.sort((a,b)=>a-b));
-	if(arr.length===1){
-		return 0;
-	}
+	
 	
     let sum=0;
     while(arr.length>1){
+		arr.sort((a,b)=>a-b)
 		let f = arr[0];
 		let s = arr[1];
 		let cost =f+s;
 		sum+=cost;
 		arr.splice(0,2);
-		let i=0;
-		while(i<arr.length && arr[i]<cost)i++;
-		arr.splice(i,0,cost)
+		arr.push(cost)
+		
 	}
     return sum
 //write your code here
